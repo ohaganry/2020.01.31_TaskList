d2d0f4adfba00af46fa55be6780181f2e1ecd0ab
ohaganry <hermits.nature@gmail.com>
2020-01-31 09:32:02 -0500

    initial black push

---
+++ tasklist/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tasklist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ tasklist/ToDo.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace tasklist
+{
+    public class ToDo
+    {
+        
+    }
+}
