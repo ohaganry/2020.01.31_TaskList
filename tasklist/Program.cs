b41a5faad689cd832e427ebc87ab64de014ef0c6
ohaganry <hermits.nature@gmail.com>
2020-02-01 11:16:12 -0500

    added features from extended exercise

--- tasklist/Program.cs
+++ tasklist/Program.cs
@@ -9,21 +9,24 @@ namespace tasklist
         static void Main(string[] args)
         {
             bool proceed = true;
-            ToDo t1 = new ToDo("Ryan", DateTime.Parse("2/3/2020"), "Complete Task List Capstone");
+            ToDo t1 = new ToDo("Ryan", DateTime.Parse("2/2/2020"), "Complete Task List Capstone");
             ToDo t2 = new ToDo("Stephen", DateTime.Parse("2/6/2020"), "Grade Student Capstones");
             ToDo t3 = new ToDo("Erik", DateTime.Parse("2/28/2020"), "Convert Task List to web/mobile app");
+            ToDo t4 = new ToDo("Ryan", DateTime.Parse("2/3/2020"), "Turn in completed task list");
 
-            List<ToDo> tasks = new List<ToDo>{t1, t2, t3};
+            List<ToDo> tasks = new List<ToDo>{t1, t2, t3, t4};
 
             while(proceed)
             {
                 Console.WriteLine("MENU\n1. List Tasks\n2. Add Task\n3. Edit Task\n4. Delete Task\n5. Mark Task Complete\n6. Quit");
-                int menu1 = Methods.CheckRange(int.Parse(Methods.GetUserInput("Choose an item form the menu")), 1, 6);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                int menu1 = Methods.CheckRange(int.Parse(Methods.GetUserInput("Choose an item from the menu")), 1, 6);
 
                 switch(menu1)
                 {
                     case 1:
                     ToDo.DisplayList(tasks);
+                    ToDo.FilterList(tasks);
                     break;
 
                     case 2:
@@ -31,6 +34,7 @@ namespace tasklist
                     break;
 
                     case 3:
+                    ToDo.EditTask(tasks);
                     break;
 
                     case 4:
@@ -39,6 +43,7 @@ namespace tasklist
 
                     case 5:
                     ToDo.DisplayList(tasks);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
                     int input = Methods.CheckRange(int.Parse(Methods.GetUserInput("Which task has been completed")), 1, tasks.Count);
                     tasks[input - 1].MarkComplete();
                     break;
@@ -47,6 +52,7 @@ namespace tasklist
                     Environment.Exit(0);
                     break;
                 }
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 proceed = Methods.ValidateInput("Would you like to continue in the task list?");
             }
         }
--- tasklist/ToDo.cs
+++ tasklist/ToDo.cs
@@ -24,26 +24,14 @@ namespace tasklist
             Complete = true;
         }
 
-        public void EditDescription(string input)
-        {
-            Description = input;
-        }
-
-        public void EditDate(DateTime date)
-        {
-            Due = date;
-        }
-
-        public void EditName(string name)
-        {
-            Name = name;
-        }
-
         public static void AddToDo(List<ToDo> listOfTasks)
         {
             ToDo newToDo = new ToDo();
+            Console.ForegroundColor = ConsoleColor.Yellow;
             newToDo.Name = Methods.GetUserInput("Who will be responsible for this task?");
+            Console.ForegroundColor = ConsoleColor.Yellow;
             newToDo.Due = DateTime.Parse(Methods.GetUserInput("When does this task need to be completed by?"));
+            Console.ForegroundColor = ConsoleColor.Yellow;
             newToDo.Description = Methods.GetUserInput("Give a brief discription of the task:");
 
             listOfTasks.Add(newToDo);
@@ -52,7 +40,9 @@ namespace tasklist
         public static void DeleteFromList(List<ToDo> listOfTasks)
         {
             DisplayList(listOfTasks);
+            Console.ForegroundColor = ConsoleColor.Yellow;
             int input = Methods.CheckRange(int.Parse(Methods.GetUserInput("Which task would you like to delete?")), 1, listOfTasks.Count);
+            Console.ForegroundColor = ConsoleColor.Red;
             bool confirm = Methods.ValidateInput("Are you sure you wish to delete this task?");
             if(confirm)
             {
@@ -67,29 +57,74 @@ namespace tasklist
         public static void DisplayList(List<ToDo> listOfTasks)
         {
             int i = 1;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" Complete    Due     Name    Description");
+            Console.ResetColor();
             foreach(ToDo t in listOfTasks)
             {
                 Console.WriteLine($"{i++}.  {t.Complete} - {t.Due.ToShortDateString()} - {t.Name} - {t.Description}");
             }
-            // int input = Methods.CheckRange(int.Parse(Methods.GetUserInput("1. Full List\n2. List for Name\n3. List before Due Date")), 1, 3);
-            // switch(input)
-            // {
-            //     case 1:
-                
-            //     break;
+        }
 
-            //     case 2:
-            //     break;
+        public static void FilterList(List<ToDo> listOfTasks)
+        {
+            int i = 1;
+            int option = Methods.CheckRange(int.Parse(Methods.GetUserInput("Filter List options\n1. Name\n2. Due Date\n3. Exit")), 1, 3);
+            switch(option)
+            {
+                case 1:
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                string name = Methods.GetUserInput("Which persons task would you like to see?");
+                foreach(ToDo t in listOfTasks)
+                {
+                    if(t.Name.ToLower() == name.ToLower())
+                    {
+                        Console.WriteLine($"{i++}.  {t.Complete} - {t.Due.ToShortDateString()} - {t.Name} - {t.Description}");
+                    }
+                    else{}
+                }
+                break;
 
-            // }
+                case 2:
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                DateTime dueDate = DateTime.Parse(Methods.GetUserInput("See tasks due before what date?"));
+                foreach(ToDo t in listOfTasks)
+                {
+                    if(t.Due <= dueDate)
+                    {
+                        Console.WriteLine($"{i++}.  {t.Complete} - {t.Due.ToShortDateString()} - {t.Name} - {t.Description}");
+                    }
+                    else{}
+                }
+                break;
+
+                case 3:
+                break;
+            }
+
         }
 
         public static void EditTask(List<ToDo> listOfTasks)
         {
            DisplayList(listOfTasks);
+           Console.ForegroundColor = ConsoleColor.Yellow;
            int taskNumber = Methods.CheckRange(int.Parse(Methods.GetUserInput("Which task do you want to edit?")), 1, listOfTasks.Count);
            int propNumber = Methods.CheckRange(int.Parse(Methods.GetUserInput("Will you change the...\n1. Name\n2. Due Date\n3. Description")), 1, 3);
-           
+           if(propNumber == 1)
+           {
+               Console.ForegroundColor = ConsoleColor.Yellow;
+               listOfTasks[taskNumber - 1].Name = Methods.GetUserInput("Who will be responsible for this task now?");
+           }
+           if(propNumber == 2)
+           {
+               Console.ForegroundColor = ConsoleColor.Yellow;
+               listOfTasks[taskNumber - 1].Due = DateTime.Parse(Methods.GetUserInput("What is the new due date? (mm/dd/yyyy)"));
+           }
+           if(propNumber == 3)
+           {
+               Console.ForegroundColor = ConsoleColor.Yellow;
+               listOfTasks[taskNumber - 1].Description = Methods.GetUserInput("Input the new task description:");
+           }
         }
 
     }
